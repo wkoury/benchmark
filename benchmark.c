59887b3cbe9c899ed9a5ad0ef39fd5ab5b7fafa4
Will Koury <wkoury@hey.com>
2020-12-07 11:57:41 -0500

    Uses more appropriate language

--- benchmark.c
+++ benchmark.c
@@ -1,22 +1,18 @@
 /* Will Koury - Project 0 */
 
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
 
     const int BIG_NUMBER = 10000000;
-    int fuckYourRAM[BIG_NUMBER];
+    int destroyYourRAM[BIG_NUMBER];
 
     for(int i = 0; i < BIG_NUMBER; ++i){
-        fuckYourRAM[i] = i;
+        destroyYourRAM[i] = i;
     }
 
     for(int i = 0; i < BIG_NUMBER; ++i){
-        fprintf(stdout, "%d\n", fuckYourRAM[i]);
+        fprintf(stdout, "%d\n", destroyYourRAM[i]);
     }
 
     return 0;
